de46f72c236947b59266d9a7c4ac77e60561cc81
fmaach <fouad_maach@yahoo.fr>
2018-11-29 13:03:28 -0800

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello World EKS FMA"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
